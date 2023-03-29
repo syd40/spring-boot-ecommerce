140740f3f72c0bf5c761a6a8a36f3f6a3445311a
syd40 <128923029+syd40@users.noreply.github.com>
2023-03-29 11:31:03 +0530

    Added Categorization of products

--- src/main/java/com/shopping/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/shopping/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,16 @@
 package com.shopping.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.shopping.ecommerce.entity.Product;
 
 @CrossOrigin("http://localhost:4200")
-public interface ProductRepository extends JpaRepository<Product, Long>{
-	
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
 }
