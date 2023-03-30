db8338e17025f2617487db5d5723b277985effb4
syd40 <128923029+syd40@users.noreply.github.com>
2023-03-30 11:46:55 +0530

    Update MyDataRestConfig.java
    added id to product_category api

--- src/main/java/com/shopping/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/shopping/ecommerce/config/MyDataRestConfig.java
@@ -1,5 +1,9 @@
 package com.shopping.ecommerce.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -9,8 +13,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import com.shopping.ecommerce.entity.Product;
 import com.shopping.ecommerce.entity.ProductCategory;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+	
+	private EntityManager entityManager;
+	
+	public MyDataRestConfig(EntityManager theEntityManager) {
+		entityManager = theEntityManager;
+	}
 
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -27,6 +40,22 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 		.forDomainType(ProductCategory.class)
 		.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
 		.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+		
+		exposeIds(config);
+	}
+
+	private void exposeIds(RepositoryRestConfiguration config) {
+		
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+		
+		List<Class<?>> entityClasses = new ArrayList<>();
+		
+		for (EntityType<?> tempEntityType : entities) {
+			entityClasses.add(tempEntityType.getJavaType());
+		}
+		
+		Class<?>[] domainTypes = entityClasses.toArray(new Class[0]);
+		config.exposeIdsFor(domainTypes);
 	}
 
 	
