fb903037bdfab3cb205d0b62f6d1d71ec78c6269
Shubham Yadav <128923029+syd40@users.noreply.github.com>
2023-04-05 06:27:59 +0530

    Added Search with product name

--- src/main/java/com/shopping/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/shopping/ecommerce/dao/ProductRepository.java
@@ -3,6 +3,7 @@ package com.shopping.ecommerce.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -12,5 +13,8 @@ import com.shopping.ecommerce.entity.Product;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+  
+	Page<Product> findByNameContainingIgnoreCase(@Param("name") String name, Pageable pageable);
 
 }
+   
